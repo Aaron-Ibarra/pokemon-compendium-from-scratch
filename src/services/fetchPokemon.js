4b0995d2ed37b27e5edfcbc072e180e15810ab22
Aaron Ibarra <aaronibarra132@gmail.com>
2023-01-23 10:20:59 -0800

    created fetch function for pokemon by name

--- src/services/fetchPokemon.js
+++ src/services/fetchPokemon.js
@@ -19,3 +19,16 @@ export async function fetchPokemon(type) {
   const data = await response.json();
   return data.results;
 }
+
+export async function fetchPokemonByName(pokemon) {
+  const params = new URLSearchParams();
+  params.set('pokemon', pokemon);
+
+  const response = await fetch(
+    `https://alchemy-pokedex.herokuapp.com/api/pokedex/?${params.toString()}`
+  );
+
+  const data = await response.json();
+
+  return data.results;
+}
